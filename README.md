f94f9625091ba2e54ff9addb49617906165e6020
Your Name <you@example.com>
2024-11-17 21:18:01 +1100

    Purchase-Page template finished

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Versz Store
+ - React + Vite
+ - Three.js for models
+ - Stripe connected for payments 
 
-# React + Vite
+
+* Basic store layout
+* button to render 3D Model
+* Stripe Payments 
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import shoppingIcon from "../public/ShoppingIcon.png"
+import VerszLogo from "../public/VerszLogo.png"
+
+import TorxBitImage from "../public/Front-RenderView.jpg"
 import "./App.css";
 
 function App() {
@@ -6,16 +10,33 @@ function App() {
     <>
       {/* Title Container*/}
       <div className="title-container">
-        <div className="title">
-          <h1 className="text-3xl font-bold">Versz Store</h1>
-        </div>
+            <img className="store-logo" src={VerszLogo}/>
+          <div className="title">
+            <h1 className="title-text text-3xl">Versz Store</h1>
+          </div>
+          <div className="title-icon-container">
+            <img className="title-icon" src={shoppingIcon}/>
+          </div>
       </div>
       {/* Main Container*/}
       <div className="main-container">
-        <div>Content</div>
+        <div className="main-container-divider">
+          <div className="content-container">
+            <div className="content-image-container">
+              <img className="content-image" src={TorxBitImage}/>
+            </div>
+            <div><button>3D Model View</button></div>
+          </div>
+          <div className="description-container">
+            <div className="text-3xl">Product Name</div>
+            <p>Product Description</p>
+            <div><button>Purchase</button></div>
+          </div>
+
+        </div>
       </div>
 
-      {/* Container 4  */}
+      {/* FooterContainer  */}
       <div className="card">
         <p>
           Developed by <code>Grahmn</code> 🛠️
