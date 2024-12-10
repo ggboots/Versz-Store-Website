6690578226cf5fadfabb7a5fcbed4d1b3b87575d
Your Name <you@example.com>
2024-12-10 18:07:49 +1100

    lil update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import shoppingIcon from "../public/ShoppingIcon.png"
-import VerszLogo from "../public/VerszLogo.png"
+import shoppingIcon from "/ShoppingIcon.png"
+import VerszLogo from "/VerszLogo.png"
 
-import TorxBitImage from "../public/Front-RenderView.jpg"
+import socketAdapterHolder from "/Front-RenderView.jpg"
 import "./App.css";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <div className="title-container">
             <img className="store-logo" src={VerszLogo}/>
           <div className="title">
-            <h1 className="title-text text-3xl">Versz Store</h1>
+            <h1 className="title-text text-3xl">VERSZ Digital</h1>
           </div>
           <div className="title-icon-container">
             <img className="title-icon" src={shoppingIcon}/>
@@ -23,14 +23,20 @@ function App() {
         <div className="main-container-divider">
           <div className="content-container">
             <div className="content-image-container">
-              <img className="content-image" src={TorxBitImage}/>
+              <img className="content-image" src={socketAdapterHolder}/>
             </div>
             <div><button>3D Model View</button></div>
           </div>
           <div className="description-container">
-            <div className="text-3xl">Product Name</div>
-            <p>Product Description</p>
-            <div><button>Purchase</button></div>
+            <div className="text-3xl mb-6 font-medium">Socket Adapter Holder (DIGITAL FILE ONLY)</div>
+            <div className="mr-6 w-5/6">
+              <p className="flex text-left">Holder for Socket adapter hex bits and socket adapter, with a magnetic base to hold position inside toolbox or metal surface</p>
+              <p className="flex text-left">This purchase only includes digital files for you to 3D print, this is a non-refundable purchase</p>
+              </div>
+            <p className="my-2 italic"> - 1x SocketAdapterMagnetBase.stl</p>
+            <p className="underline mt-2 mb-6">Refund Policy </p>
+            <div className="mt-8 w-3/4 flex justify-center"><button>Add to Cart</button></div>
+            {/* <div className="stripe-button-container"><button className="bg-purple-600 text-neutral-100 font-bold "><h2 >Stripe </h2></button></div> */}
           </div>
 
         </div>
